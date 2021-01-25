1fd1f3e48004f9049f073d388f86d43879c3d8f5
grigory <grigory.timofeev@saritasa.com>
2021-01-25 19:15:06 +0700

    Finish work on ColorMyViews app

--- Aboutme/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ Aboutme/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -4,11 +4,7 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.inputmethod.InputBinding
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.example.aboutme.databinding.ActivityMainBinding
 
